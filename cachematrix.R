dccea7fbec8c1f4a3e2e882f8e414cd08552b820
Viacheslav Rodionov <viacheslav.rodionov@gmail.com>
2015-11-22 23:15:24 +0100

    initial solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() x
+  setcache <- function(calculated) cache <<- calculated
+  getcache <- function() cache
+  list(set = set, get = get,
+       setcache = setcache,
+       getcache = getcache)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getcache()
+  if(!is.null(inv)) {
+    #print("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setcache(inv)
+  inv
 }
+
+## you may check my code by removing comments and debugging the code below:
+
+## make cache object
+#cm <- makeCacheMatrix(m)
+#n = 1000
+## create random inversible matrix
+#m = diag(runif(n))
+## put matrix into a cache
+#cm$set(m)
+#inv1 <- cacheSolve(cm)
+## this call goes into a cache
+#inv2 <- cacheSolve(cm)
+
